32fb1c98586ca9fd09add81e09823b3cd56107f8
Brandon Guffey <hypefox@gmail.com>
2020-04-07 13:46:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,12 @@
 class MP3Importer
-  
-  attr_accessor :path 
-  
+  attr_reader :path, :files
+
   def initialize(path)
     @path = path
+    @files = Dir.entries(path).grep(/.*\.mp3/)
   end
-  
-  def files 
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-  end
-  
-  def import 
-    files.each {|song| Song.new_by_filename(song)}
+
+  def import
+    @files.each {|file| Song.new_by_filename(file)}
   end
 end
